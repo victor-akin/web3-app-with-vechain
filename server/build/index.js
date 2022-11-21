1dd844bb54029697a47055a5f741c529d029c651
Victor Akinyokun <vicgbadebo@gmail.com>
2022-11-21 09:44:29 +0100

    project-init

---
+++ server/build/index.js
@@ -0,0 +1,9 @@
+import * as http from 'http';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const server = http.createServer((req, res) => {
+    res.end('Hello World');
+});
+server.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server running at ${process.env.SERVER_PORT}/`);
+});
